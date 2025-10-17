7224b81963da6db095f7f1d24796ef43017616e9
SortyFix <yonasnieder@gmail.com>
2025-10-17 15:45:27 +0200

    Refactor RepositoryModel to include id and files properties
    Updated RepositoryModel to accept `id` and `files` properties and implemented appropriate overrides for `equals`, `hashCode`, and `toString`. This improves data handling and ensures proper behavior for object comparisons and string representation.

--- EEDU-Backend/src/main/java/de/gaz/eedu/upload/RepositoryModel.java
+++ EEDU-Backend/src/main/java/de/gaz/eedu/upload/RepositoryModel.java
@@ -1,26 +1,39 @@
 package de.gaz.eedu.upload;
 
 import de.gaz.eedu.entity.model.EntityModel;
+import de.gaz.eedu.upload.file.FileModel;
 import org.jetbrains.annotations.NotNull;
 
-public record RepositoryModel() implements EntityModel<Long>
-{
+import java.util.Objects;
+import java.util.Set;
 
+public record RepositoryModel(@NotNull Long id, @NotNull Set<FileModel> files) implements EntityModel<Long>
+{
     @Override
     public @NotNull Long id()
     {
-        return 0;
+        return id;
     }
 
-    @Override
-    public boolean equals(Object obj)
+    @Override public boolean equals(Object o)
     {
-        return false;
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RepositoryModel that = (RepositoryModel) o;
+        return Objects.equals(id(), that.id()) && Objects.equals(files(), that.files());
+    }
+
+    @Override public String toString()
+    {
+        return "RepositoryModel{" +
+                "id=" + id() +
+                ", files=" + files() +
+                '}';
     }
 
     @Override
     public int hashCode()
     {
-        return 0;
+        return Objects.hash(id);
     }
 }
